71d5fe00e79899fcac7dbc67c69456104632bc4b
guicybercode <euguigom@gmail.com>
2025-11-19 13:55:43 -0300

    Update project dependencies

--- phoenix_live/mix.exs
+++ phoenix_live/mix.exs
@@ -41,6 +41,9 @@ defmodule PhoenixLive.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.8.1"},
+      {:phoenix_ecto, "~> 4.2"},
+      {:ecto_sql, "~> 3.6"},
+      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0"},
